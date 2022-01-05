6ac0ab3952ac1b7b127d74b6c39ec42a87fd08de
arusc <alex.sa4ever@hotmail.t>
2022-01-05 22:40:54 +0100

    Evento e Struttura Testing

--- src/test/java/model/evento/EventoDAOTest.java
+++ src/test/java/model/evento/EventoDAOTest.java
@@ -7,13 +7,16 @@ import org.junit.Test;
 import java.sql.Date;
 import java.sql.Time;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class EventoDAOTest {
-
     private EventoDAO eventoDAO;
+    private EventoServiceImpl eventoService;
+
     @Before
     public void setUp(){
         eventoDAO=new EventoDAO();
+        eventoService=new EventoServiceImpl();
     }
 
     @Test
@@ -26,7 +29,7 @@ public class EventoDAOTest {
     @Test
     public void addEventoTest() {
         Evento e=new Evento();
-        e.setIdEvento(100);
+        e.setIdEvento(eventoService.visualizzaEventi().size()+1);
         e.setNome("Nuovo Evento");
         e.setNumeroPartecipanti(12);
         Date data=Date.valueOf("2021-12-25");
@@ -38,12 +41,50 @@ public class EventoDAOTest {
         e.setStruttura(s);
         eventoDAO.addEvento(e);
 
-        Evento test= eventoDAO.doRetrieveById(100);
+        Evento test= eventoDAO.doRetrieveById(eventoService.visualizzaEventi().size());
         assertEquals(test.getIdEvento(),e.getIdEvento());
         assertEquals(test.getNome(),e.getNome());
         assertEquals(test.getNumeroPartecipanti(),e.getNumeroPartecipanti());
         assertEquals(test.getDataEvento(),e.getDataEvento());
         assertEquals(test.getOrario(),e.getOrario());
         assertEquals(test.getStruttura().getIdStruttura(),e.getStruttura().getIdStruttura());
+    }
+
+    @Test
+    public void doChangesTest(){
+        Evento e=new Evento();
+        e.setIdEvento(eventoService.visualizzaEventi().size());
+        e.setNome("Nuovo Eventissimo");
+        e.setNumeroPartecipanti(12);
+        Date data=Date.valueOf("2021-12-25");
+        e.setDataEvento(data);
+        Time t=new Time(10,10,10);
+        e.setOrario(t);
+        Struttura s=new Struttura();
+        s.setIdStruttura(1);
+        e.setStruttura(s);
+        eventoDAO.doChanges(e);
+
+        Evento test= eventoDAO.doRetrieveById(eventoService.visualizzaEventi().size());
+        assertEquals(test.getIdEvento(),e.getIdEvento());
+        assertEquals(test.getNome(),e.getNome());
+        assertEquals(test.getNumeroPartecipanti(),e.getNumeroPartecipanti());
+        assertEquals(test.getDataEvento(),e.getDataEvento());
+        assertEquals(test.getOrario(),e.getOrario());
+        assertEquals(test.getStruttura().getIdStruttura(),e.getStruttura().getIdStruttura());
+    }
+
+    @Test
+    public void deleteEventoTest(){
+        int id=eventoService.visualizzaEventi().size();
+        eventoDAO.deleteById(id);
+
+        Evento test= eventoDAO.doRetrieveById(id);
+        assertEquals(0,test.getIdEvento());
+        assertNull(test.getNome());
+        assertEquals(0,test.getNumeroPartecipanti());
+        assertNull(test.getDataEvento());
+        assertNull(test.getOrario());
+        assertNull(test.getStruttura());
     }
 }
--- src/test/java/model/struttura/StrutturaDAOTest.java
+++ src/test/java/model/struttura/StrutturaDAOTest.java
@@ -1,14 +1,19 @@
 package model.struttura;
 
+import model.evento.Evento;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class StrutturaDAOTest {
     private StrutturaDAO strutturaDAO;
+    private StrutturaServiceImpl strutturaService;
+
     @Before
     public void setUp(){
         strutturaDAO=new StrutturaDAO();
+        strutturaService=new StrutturaServiceImpl();
     }
 
     @Test
@@ -17,4 +22,74 @@ public class StrutturaDAOTest {
         Struttura s=strutturaDAO.doRetrieveById(id);
         assertEquals(id,s.getIdStruttura());
     }
+
+    @Test
+    public void addStrutturaTest(){
+        Struttura s=new Struttura();
+        s.setIdStruttura(strutturaService.visualizzaStrutture().size()+1);
+        s.setNome("Struttura");
+        s.setIndirizzo("via roma");
+        s.setTelefono("089331029");
+        s.setDescrizione("descrizione");
+        s.setCapienza(100);
+        s.setNumeroSpogliatoi(2);
+        s.setParcheggio(true);
+        s.setCategoria("Calcio");
+        strutturaDAO.addStruttura(s);
+
+        Struttura test=strutturaDAO.doRetrieveById(strutturaService.visualizzaStrutture().size());
+        assertEquals(test.getIdStruttura(),s.getIdStruttura());
+        assertEquals(test.getNome(),s.getNome());
+        assertEquals(test.getIndirizzo(),s.getIndirizzo());
+        assertEquals(test.getTelefono(),s.getTelefono());
+        assertEquals(test.getDescrizione(),s.getDescrizione());
+        assertEquals(test.getCapienza(),s.getCapienza());
+        assertEquals(test.getNumeroSpogliatoi(),s.getNumeroSpogliatoi());
+        assertEquals(test.isParcheggio(),s.isParcheggio());
+        assertEquals(test.getCategoria(),s.getCategoria());
+    }
+
+    @Test
+    public void doChangesTest(){
+        Struttura s=new Struttura();
+        s.setIdStruttura(strutturaService.visualizzaStrutture().size());
+        s.setNome("Strutturona");
+        s.setIndirizzo("via roma");
+        s.setTelefono("089331029");
+        s.setDescrizione("descrizione");
+        s.setCapienza(100);
+        s.setNumeroSpogliatoi(2);
+        s.setParcheggio(true);
+        s.setCategoria("Calcio");
+        strutturaDAO.doChanges(s);
+
+        Struttura test=strutturaDAO.doRetrieveById(strutturaService.visualizzaStrutture().size());
+        assertEquals(test.getIdStruttura(),s.getIdStruttura());
+        assertEquals(test.getNome(),s.getNome());
+        assertEquals(test.getIndirizzo(),s.getIndirizzo());
+        assertEquals(test.getTelefono(),s.getTelefono());
+        assertEquals(test.getDescrizione(),s.getDescrizione());
+        assertEquals(test.getCapienza(),s.getCapienza());
+        assertEquals(test.getNumeroSpogliatoi(),s.getNumeroSpogliatoi());
+        assertEquals(test.isParcheggio(),s.isParcheggio());
+        assertEquals(test.getCategoria(),s.getCategoria());
+    }
+
+    @Test
+    public void deleteStrutturaTest(){
+        int id=strutturaService.visualizzaStrutture().size();
+        strutturaDAO.deleteById(id);
+
+        Struttura test= strutturaDAO.doRetrieveById(id);
+        assertNull(test.getNome());
+        assertNull(test.getDescrizione());
+        assertNull(test.getCategoria());
+        assertNull(test.getTelefono());
+        assertNull(test.getIndirizzo());
+        assertEquals(0,test.getCapienza());
+        assertEquals(0,test.getIdStruttura());
+        assertEquals(0,test.getNumeroSpogliatoi());
+    }
+
+
 }
