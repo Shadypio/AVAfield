56de0df65d7e6332822910dc91ba6c54ffa7ea9b
Vincenzopio Amendola <enzuccioamendola@gmail.com>
2022-01-05 22:40:12 +0100

    Testing RecensioneDAO e UtenteDao

--- src/test/java/model/recensione/RecensioneDAOTest.java
+++ src/test/java/model/recensione/RecensioneDAOTest.java
@@ -1,15 +1,21 @@
 package model.recensione;
 
+import model.struttura.Struttura;
+import model.utente.Utente;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class RecensioneDAOTest {
     private RecensioneDAO recensioneDAO;
+    private RecensioneServiceImpl recensioneService;
+
     @Before
     public void setUp(){
         recensioneDAO=new RecensioneDAO();
+        recensioneService=new RecensioneServiceImpl();
     }
 
     @Test
@@ -17,5 +23,74 @@ public class RecensioneDAOTest {
         int id=1;
         Recensione r=recensioneDAO.doRetrieveById(id);
         assertEquals(id,r.getIdRecensione());
+    }
+
+    @Test
+    public void addRecensioneTest() {
+        Recensione r = new Recensione();
+        r.setIdRecensione(recensioneService.visualizzaRecensioni().size()+1);
+        r.setTitolo("TitoloDiTest");
+        r.setTesto("TestoDiTest");
+        r.setNumeroStelle(5);
+
+        Utente u = new Utente();
+        u.setIdUtente(1);
+        r.setUtente(u);
+
+        Struttura s = new Struttura();
+        s.setIdStruttura(1);
+        r.setStruttura(s);
+
+        recensioneDAO.addRecensione(r);
+
+        Recensione test = recensioneDAO.doRetrieveById(recensioneService.visualizzaRecensioni().size());
+        assertEquals(test.getIdRecensione(), r.getIdRecensione());
+        assertEquals(test.getTesto(), r.getTesto());
+        assertEquals(test.getTitolo(), r.getTitolo());
+        assertEquals(test.getNumeroStelle(), r.getNumeroStelle());
+        assertEquals(test.getUtente().getIdUtente(), r.getUtente().getIdUtente());
+        assertEquals(test.getStruttura().getIdStruttura(), test.getStruttura().getIdStruttura());
+    }
+
+    @Test
+    public void doChangesRecensioneTest() {
+        Recensione r = new Recensione();
+        r.setIdRecensione(recensioneService.visualizzaRecensioni().size());
+        r.setTitolo("TitoloDiTest");
+        r.setTesto("TestoDiTest");
+        r.setNumeroStelle(5);
+
+        Utente u = new Utente();
+        u.setIdUtente(1);
+        r.setUtente(u);
+
+        Struttura s = new Struttura();
+        s.setIdStruttura(1);
+        r.setStruttura(s);
+
+        recensioneDAO.doChanges(r);
+
+        Recensione test = recensioneDAO.doRetrieveById(recensioneService.visualizzaRecensioni().size());
+        assertEquals(test.getIdRecensione(), r.getIdRecensione());
+        assertEquals(test.getTesto(), r.getTesto());
+        assertEquals(test.getTitolo(), r.getTitolo());
+        assertEquals(test.getNumeroStelle(), r.getNumeroStelle());
+        assertEquals(test.getUtente().getIdUtente(), r.getUtente().getIdUtente());
+        assertEquals(test.getStruttura().getIdStruttura(), test.getStruttura().getIdStruttura());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        int id = recensioneService.visualizzaRecensioni().size();
+        recensioneDAO.deleteById(id);
+
+        Recensione test = recensioneDAO.doRetrieveById(id);
+        assertEquals(test.getIdRecensione(), 0);
+        assertNull(test.getTitolo());
+        assertNull(test.getTesto());
+        assertEquals(test.getNumeroStelle(), 0);
+        assertNull(test.getUtente());
+        assertNull(test.getStruttura());
+
     }
 }
--- src/test/java/model/utente/UtenteDAOTest.java
+++ src/test/java/model/utente/UtenteDAOTest.java
@@ -1,15 +1,24 @@
 package model.utente;
 
+import model.recensione.Recensione;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class UtenteDAOTest {
 
     private UtenteDAO utenteDAO;
+    private UtenteServiceImpl utenteService;
+
     @Before
     public void setUp(){
+
         utenteDAO=new UtenteDAO();
+        utenteService = new UtenteServiceImpl();
     }
 
     @Test
@@ -17,5 +26,77 @@ public class UtenteDAOTest {
         int id=1;
         Utente u=utenteDAO.doRetrieveById(id);
         assertEquals(id,u.getIdUtente());
+    }
+
+    @Test
+    public void addUtenteTest() {
+        Utente u = new Utente();
+        u.setIdUtente(utenteService.visualizzaUtenti().size()+1);
+        u.setNome("NomeTest");
+        u.setCognome("CognomeTest");
+        u.setUsername("UsernameTest");
+        u.setEmail("EmailTest");
+        u.setPassword("PasswordTest");
+        u.setAdmin(false);
+        u.setNumeroTelefono("1234567890");
+        u.setAutovalutazione(4);
+
+        utenteDAO.addUtente(u);
+
+        Utente test = utenteDAO.doRetrieveById(utenteService.visualizzaUtenti().size());
+        assertEquals(test.getIdUtente(), u.getIdUtente());
+        assertEquals(test.getNome(), u.getNome());
+        assertEquals(test.getCognome(), u.getCognome());
+        assertEquals(test.getEmail(), u.getEmail());
+        assertEquals(test.getUsername(), u.getUsername());
+        //assertEquals(test.getPassword(), u.getPassword());
+        assertEquals(test.isAdmin(), u.isAdmin());
+        assertEquals(test.getNumeroTelefono(), u.getNumeroTelefono());
+        assertEquals(test.getAutovalutazione(), u.getAutovalutazione());
+    }
+
+    @Test
+    public void doChangesUtenteTest() {
+        Utente u = new Utente();
+        u.setIdUtente(utenteService.visualizzaUtenti().size());
+        u.setNome("NomeTest");
+        u.setCognome("CognomeTest");
+        u.setUsername("UsernameTest");
+        u.setEmail("EmailTest");
+        u.setPassword("PasswordTest");
+        u.setAdmin(false);
+        u.setNumeroTelefono("1234567890");
+        u.setAutovalutazione(4);
+
+        utenteDAO.doChanges(u);
+
+        Utente test = utenteDAO.doRetrieveById(utenteService.visualizzaUtenti().size());
+        assertEquals(test.getIdUtente(), u.getIdUtente());
+        assertEquals(test.getNome(), u.getNome());
+        assertEquals(test.getCognome(), u.getCognome());
+        assertEquals(test.getEmail(), u.getEmail());
+        assertEquals(test.getUsername(), u.getUsername());
+        //assertEquals(test.getPassword(), u.getPassword());
+        assertEquals(test.isAdmin(), u.isAdmin());
+        assertEquals(test.getNumeroTelefono(), u.getNumeroTelefono());
+        assertEquals(test.getAutovalutazione(), u.getAutovalutazione());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        int id = utenteService.visualizzaUtenti().size();
+        utenteDAO.deleteById(id);
+
+        Utente test = utenteDAO.doRetrieveById(id);
+        assertEquals(test.getIdUtente(), 0);
+        assertNull(test.getNome());
+        assertNull(test.getCognome());
+        assertNull(test.getEmail());
+        assertNull(test.getPassword());
+        assertNull(test.getUsername());
+        assertEquals(test.isAdmin(), false);
+        assertNull(test.getNumeroTelefono());
+        assertEquals(test.getAutovalutazione(), 0);
+
     }
 }
